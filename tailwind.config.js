3d57ddd86d7f3fc0f5a25050f6b72bf2bd63c7bc
Hans Opoku <88454205+HansOheneba@users.noreply.github.com>
2023-07-26 14:37:41 +0000

    font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,8 @@ module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
     extend: {
-      height: {
-        50: "50rem",
+      colors: {
+        customBlue: '#011326',
       },
     },
   },
