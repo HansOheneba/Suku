f777689535fe8216175adf42217d461a7a4ca62b
Hans Opoku <88454205+HansOheneba@users.noreply.github.com>
2023-08-07 13:23:09 +0000

    attributes section

--- dist/app.js
+++ dist/app.js
@@ -1,11 +1,40 @@
-<script>
-  // Get the menu button and the navigation links
-  const menuBtn = document.getElementById('menuBtn');
-    const navLinks = document.querySelector('ul');
 
-    if (menuBtn && navLinks) {
-      menuBtn.addEventListener('click', () => {
-    // Toggle the 'hidden' class on the navigation links
-        navLinks.classList.toggle('hidden');
-      });
-</script>
+(function () {
+  const header = document.querySelector('.header');
+  const icon = document.querySelector('.icon-container');
+
+  icon.onclick = function () {
+    header.classList.toggle('menu-open');
+   };
+})();
+
+
+  window.addEventListener('DOMContentLoaded', function () {
+    const carousel = document.querySelector('.carousel');
+    const slides = document.querySelectorAll('.carousel-slide');
+    const prevBtn = document.querySelector('.prev-btn');
+    const nextBtn = document.querySelector('.next-btn');
+
+    let slideIndex = 0;
+
+    function showSlide(index) {
+      if (index < 0) {
+        slideIndex = slides.length - 1;
+      } else if (index >= slides.length) {
+        slideIndex = 0;
+      }
+
+      carousel.style.transform = `translateX(-${slideIndex * 100}%)`;
+    }
+
+    prevBtn.addEventListener('click', () => {
+      showSlide(--slideIndex);
+    });
+
+    nextBtn.addEventListener('click', () => {
+      showSlide(++slideIndex);
+    });
+
+    showSlide(slideIndex); // Show the initial slide
+  });
+
